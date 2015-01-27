27/01/2015: Incorporated lots into MFI_INFO;MFI bandpasses, Frequency centres, Average R-factors and new colour corrected apparent diode temperatures. Updated lm_sourcefit to allow for more control over beams. SF_DiodeTemp.py script made more general and cleaned up code. SF_Fit changed to reflect Riccardo's measured beam sizes.

DataAccess/ReadObs.py
@@ -48,7 +48,7 @@ def Count(filelist,keys,dir=''):
 
     return dShapes,len(filelist)
 
-def FullObs(filelist,keys,dir=''):
+def FullObs(filelist,keys,dir='',colids = None):
     '''
     Return dictionary of data from many Table Fits files
 
@@ -56,27 +56,38 @@ def FullObs(filelist,keys,dir=''):
     filename -- Name of table fits file to access
     keys -- Array of keys to pull from table fits file
 
+    Keyword Arguments
+    colids -- If a list is passed, only the column indicated for each key will be returned. (Default: None)
+
     '''
 
     dShapes,nFiles = Count(filelist,keys,dir=dir)
 
     #Setup data containers
+    if type(colids) != type(None):
+        for i in range(len(dShapes)):
+            dShapes[i][2] = 1
+        
     data = {keys[i]:np.zeros(dShapes[i]) for i in range(len(keys))}
 
+    #Columns can have different lengths:
     lastLen = np.zeros(len(keys),dtype='i')
 
     #Read in the data properly
     print 'TOTAL NUMBER OF FILES:',len(filelist)
     print 'OPEN: '
     for j,f in enumerate(filelist):
-        print j
+        print f, j
         hdu = pyfits.open(dir+f)
 
         for ik,k in enumerate(keys):
             thisLen   = hdu[1].data[k].shape[1]
-            print lastLen[ik],thisLen,lastLen[ik]+thisLen
-            print data[k].shape
-            data[k][0,lastLen[ik]:lastLen[ik]+thisLen,:] = hdu[1].data[k][0]
+
+            if type(colids) != type(None):            
+                data[k][0,lastLen[ik]:lastLen[ik]+thisLen,0] = hdu[1].data[k][0,:,colids[ik]]
+            else:
+                data[k][0,lastLen[ik]:lastLen[ik]+thisLen,:] = hdu[1].data[k][0]
+                
             lastLen[ik]  += thisLen
 
         hdu.close()
@@ -86,6 +97,8 @@ def FullObs(filelist,keys,dir=''):
     print '---'
     return data
 
+#print lastLen[ik],thisLen,lastLen[ik]+thisLen
+#print data[k].shape
 
 def SingleObs(filename,keys,dir=''):
     '''
@@ -112,6 +125,68 @@ def SingleObs(filename,keys,dir=''):
 
     hdu.close()
     del hdu
+
+    print ''
+    print '---'
+    return data
+
+
+import Coordinates
+import healpy as hp
+
+def FullObsMapping(filelist,keys,TPoints,ch,jd0=56244.,nside = 512,dir=''):
+    '''
+    A special form of FullObs specifically for Map-Making to keep memory use low:
+    Expects the following keys: ["DATA","MASK","AZ","DEC","JD"]
+    
+    
+    Return dictionary of data from many Table Fits files
+
+    Arguments
+    filename -- Name of table fits file to access
+    keys -- Array of keys to pull from table fits file
+
+    Keyword Arguments
+    colids -- If a list is passed, only the column indicated for each key will be returned. (Default: None)
+
+    '''
+
+    dShapes,nFiles = Count(filelist,keys,dir=dir)
+
+    #Setup data containers
+    data = {'TOD':np.zeros((1,dShapes[0][1],1)),
+            'PIX':np.zeros((1,dShapes[0][1],1)),
+            'MASK':np.zeros((1,dShapes[0][1],1),dtype='bool')}
+
+
+    #Read in the data properly
+    print 'TOTAL NUMBER OF FILES:',len(filelist)
+    print 'OPEN: '
+
+    lastLen = 0
+    for j,f in enumerate(filelist):
+        print f, j
+        hdu = pyfits.open(dir+f)
+
+        thisLen   = hdu[1].data['DATA'].shape[1]
+
+        
+        data['TOD'][0,lastLen:lastLen+thisLen,0] = hdu[1].data['DATA'][0,:,ch]
+
+        ra,dec,p = Coordinates.Hor2Sky(hdu[1].data['AZ'][0,:,0]*np.pi/180.,
+                                       hdu[1].data['EL'][0,:,0]*np.pi/180.,
+                                       hdu[1].data['JD'][0,:,0]+jd0,TPoints=TPoints)
+
+        data['MASK'][0,lastLen:lastLen+thisLen,0] = hdu[1].data['MASK'][0,:,ch].astype('bool') & (dec > 0.) 
+
+        pix = hp.ang2pix(nside,np.pi/2.-dec,ra)
+
+        data['PIX'][0,lastLen:lastLen+thisLen,0] = pix
+        lastLen  += thisLen
+
+        hdu.close()
+        del hdu
+
 
     print ''
     print '---'

Scripts/SF_DiodeTemp.py
@@ -12,85 +12,141 @@ from matplotlib import pyplot
 import CalFitting
 import WaveFitter
 import Binning
+import DataAccess
 
 import SourceFitting
 from SourceFitting.ModelFlux import toJy
 
+def SourceFlux(nu_bp,G_bp,mjd,srcs):
+    '''
+    Return list of fluxes for Tau A or Cas A
+    '''
+    #First determine central frequency:
+    dnu = nu_bp[1] - nu_bp[0]
+    nu_c = np.sum(nu_bp*G_bp)/np.sum(G_bp)
+    BW = nu_bp*np.sum(G_bp)**2/np.sum(G_bp**2)
 
+
+    #Central frequency of Tau A:
+    spec_crab = SourceFitting.ModelFlux.TauAFlux(nu_bp,mjd[0])
+    nu_c_crab = np.sum(nu_bp*G_bp*spec_crab)/np.sum(G_bp*spec_crab)
+
+    #Central frequency of Cas A:
+    spec_casa = SourceFitting.ModelFlux.CasAFlux(nu_bp,mjd[0])
+    nu_c_casa = np.sum(nu_bp*G_bp*spec_casa)/np.sum(G_bp*spec_casa)     
+
+    #Which vals are Cas/Tau:
+    crabs = np.where((srcs == 'CRAB'))[0]
+    casas = np.where((srcs == 'CASS'))[0]
+
+    fluxs = np.zeros(len(srcs))
+    corfact = np.zeros(len(srcs))
+
+    fluxs[crabs] = SourceFitting.ModelFlux.TauAFlux(nu_c,mjd[crabs])
+    fluxs[casas] = SourceFitting.ModelFlux.CasAFlux(nu_c,mjd[casas])
+
+    #Ratio between 
+    corfact[crabs] = SourceFitting.ModelFlux.TauAFlux(nu_c,mjd[crabs])/SourceFitting.ModelFlux.TauAFlux(nu_c_crab,mjd[crabs])
+    corfact[casas] = SourceFitting.ModelFlux.CasAFlux(nu_c,mjd[casas])/SourceFitting.ModelFlux.CasAFlux(nu_c_casa,mjd[casas])
+
+
+    return fluxs,corfact
+
+def ReadBandpasses(mfidir):
+
+    #Read in bandpasses:
+    bandpasses = [np.loadtxt(mfidir+'Pol2_bandpass.dat',skiprows=1),
+                  np.loadtxt(mfidir+'Pol3_bandpass.dat',skiprows=1),
+                  np.loadtxt(mfidir+'Pol4_bandpass.dat',skiprows=1)]
+    
+    #Read in avg-rfactors
+    rfactors = np.loadtxt(mfidir+'AvgRFactors.dat')
+
+    freqs = bandpasses[0][:,0]
+    band1 = np.array([bandpasses[0][:,1] + rfactors[4  ]*bandpasses[0][:,7],
+                      bandpasses[0][:,2] + rfactors[5  ]*bandpasses[0][:,8],
+                      bandpasses[0][:,3] + rfactors[6  ]*bandpasses[0][:,5],
+                      bandpasses[0][:,4] + rfactors[7  ]*bandpasses[0][:,6],
+                      bandpasses[1][:,1] + rfactors[4+4]*bandpasses[1][:,7],
+                      bandpasses[1][:,2] + rfactors[5+4]*bandpasses[1][:,8],
+                      bandpasses[1][:,3] + rfactors[6+4]*bandpasses[1][:,5],
+                      bandpasses[1][:,4] + rfactors[7+4]*bandpasses[1][:,6],
+                      bandpasses[2][:,1] + rfactors[4+8]*bandpasses[2][:,7],
+                      bandpasses[2][:,2] + rfactors[5+8]*bandpasses[2][:,8],
+                      bandpasses[2][:,3] + rfactors[6+8]*bandpasses[2][:,5],
+                      bandpasses[2][:,4] + rfactors[7+8]*bandpasses[2][:,6]])
+
+    return [freqs,band1]
+
+    
 if __name__ == '__main__':
 
-    n = np.loadtxt('TauA',dtype='string')
+    mfidir = '/nas/scratch/sharper/QUIJOTE/MFI-Pipeline/MFI_INFO/'
+
+    #Define telescope constants:
     jd0 = 56244.
-
-    print n.shape
-
-    beam13 = (0.66*np.pi/180.)**2#(0.88*np.pi/180.)**2#
-    beam11 = (0.66*np.pi/180.)**2#(0.86*np.pi/180.)**2#
-    f1 = 19.
-    f2 = 17.
-    h = 2
-
-    gd = ((n[:,5].astype('f') != 0.))
-
-    CasFlux13GHz = SourceFitting.ModelFlux.TauAFlux(f1,n[gd,26].astype('f')+jd0)
-    CasFlux11GHz = SourceFitting.ModelFlux.TauAFlux(f2,n[gd,26].astype('f')+jd0)
-
-    #pyplot.hist(CasFlux13GHz/n[:,1].astype('f')/ toJy(f1,beam13))
-    #pyplot.hist(CasFlux13GHz/n[:,3].astype('f')/ toJy(f2,beam13),alpha=0.6)
-
-    #Calculate stability of correlated channels
-    cTd11 = np.median(CasFlux11GHz/n[gd,2+h*4].astype('f')/ toJy(f2,beam11))
-    stdcTd11 = np.std(CasFlux11GHz/n[gd,2+h*4].astype('f')/ toJy(f2,beam11))    
-    cTd13 = np.median(CasFlux13GHz/n[gd,1+h*4].astype('f')/ toJy(f1,beam13))
-    stdcTd13 = np.std(CasFlux13GHz/n[gd,1+h*4].astype('f')/ toJy(f1,beam13))
-
-    print '11GHz: ',cTd11,stdcTd11,stdcTd11/cTd11 * 100.
-    print '13GHz: ',cTd13,stdcTd13,stdcTd13/cTd13 * 100.
-    print np.log10(cTd11/cTd13)/np.log10(f2/f1)
-
-    #Calculate stability of uncorrelated channels
-    uTd11 = np.median(CasFlux11GHz/n[gd,4+h*4].astype('f')/ toJy(f2,beam11))
-    stduTd11 = np.std(CasFlux11GHz/n[gd,4+h*4].astype('f')/ toJy(f2,beam11))    
-    uTd13 = np.median(CasFlux13GHz/n[gd,3+h*4].astype('f')/ toJy(f1,beam13))
-    stduTd13 = np.std(CasFlux13GHz/n[gd,3+h*4].astype('f')/ toJy(f1,beam13))
-
-    print '11GHz: ',uTd11,stduTd11,stduTd11/uTd11 * 100.
-    print '13GHz: ',uTd13,stduTd13,stduTd13/uTd13 * 100.
-    print np.log10(uTd11/uTd13)/np.log10(f2/f1)
-
-    #b = CasFlux13GHz/n[gd,3+h*4].astype('f')/ toJy(f1,beam13)
-    #bd = ( (b > 2))
-    #print n[bd,0]
-    #pyplot.plot(n[gd,26].astype('f')+jd0,n[gd,3+h*4].astype('f'),'o')    
-    #pyplot.plot(CasFlux13GHz/n[gd,7].astype('f')/ toJy(f1,beam13),'o')
-    #pyplot.show()
-
-    pyplot.plot(n[gd,26].astype('f')+jd0,CasFlux13GHz/n[gd,1+h*4].astype('f')/ toJy(f1,beam13),'o',label='Correlated 19GHz',alpha=0.8)
-    pyplot.plot(n[gd,26].astype('f')+jd0,CasFlux11GHz/n[gd,4+h*4].astype('f')/ toJy(f2,beam11),'o',label='Uncorrelated 17GHz',alpha=0.8)
-    pyplot.plot(n[gd,26].astype('f')+jd0,CasFlux13GHz/n[gd,3+h*4].astype('f')/ toJy(f1,beam13),'o',label='Uncorrelated 19GHz',alpha=0.8)
-    pyplot.plot(n[gd,26].astype('f')+jd0,CasFlux11GHz/n[gd,2+h*4].astype('f')/ toJy(f2,beam11),'o',label='Correlated 17GHz',alpha=0.8)
-
-    ax = pyplot.gca()
-    ylims = ax.get_ylim()
-    pyplot.plot([56757,56757],[0.,ylims[1]*2.],'--k',linewidth=3)
-    pyplot.ylim(ylims[0],ylims[1])
-
-    pyplot.xlabel(r'MJD')
-    pyplot.ylabel(r'Antenna Temperature (K)')
-    pyplot.legend(numpoints=1)
-
-    pyplot.figure()
-    pyplot.plot(n[gd,26].astype('f')+jd0,n[gd,1+h*4+12].astype('f'),'o',label='Correlated 19GHz')
-    pyplot.plot(n[gd,26].astype('f')+jd0,n[gd,4+h*4+12].astype('f'),'o',label='Uncorrelated 17GHz')
-    pyplot.plot(n[gd,26].astype('f')+jd0,n[gd,3+h*4+12].astype('f'),'o',label='Uncorrelated 19GHz')
-    pyplot.plot(n[gd,26].astype('f')+jd0,n[gd,2+h*4+12].astype('f'),'o',label='Correlated 17GHz')
-
-    ax = pyplot.gca()
-    ylims = ax.get_ylim()
-    pyplot.plot([56757,56757],[0.,ylims[1]*2.],'--k',linewidth=3)
-    pyplot.ylim(ylims[0],ylims[1])
     
-    pyplot.xlabel(r'MJD')
-    pyplot.ylabel(r'r-factor',style='italic')
-    pyplot.legend(numpoints=1)    
-    pyplot.show()
+    beams = [0.92*(0.85*np.pi/180.)**2,0.99*(0.64*np.pi/180.)**2] #Beams of H2 to H4
+    freqs = [[12.891,11.605,12.881,11.149],
+             [12.891,11.605,12.881,11.149],
+             [12.891,11.605,12.881,11.149]] #Freqs of H2 to H4
+
+    channels = np.arange(12,dtype='i')
+    chan = ['H219c','H217c','H219u','H217u','H313c','H311c','H313u','H311u','H419c','H417c','H419u','H417u']
+
+    #Read in bandpasses:
+    bandpasses = ReadBandpasses(mfidir)
+
+    #Read in the data:
+    #crab = np.loadtxt('TauA',dtype='string')
+    crab = np.loadtxt('TauA',dtype='string')   
+    casa = np.loadtxt('CasA',dtype='string')
+
+    el    = np.append(crab[:,25].astype('f'),casa[:,25].astype('f')) + jd0
+    mjd   = np.append(crab[:,26].astype('f'),casa[:,26].astype('f')) + jd0
+    peaks = np.reshape(np.append(crab[:,1:13].astype('f'),casa[:,1:13].astype('f')),(len(mjd),12))
+    srcs  = np.append(np.array([c[0:4] for c in crab[:,0]]),
+                      np.array([c[0:4] for c in casa[:,0]]))
+    dios  = np.reshape(np.append(crab[:,1+12:13+12].astype('f'),casa[:,1+12:13+12].astype('f')),(len(mjd),12))
+    para  = np.append(crab[:,27].astype('f'),casa[:,27].astype('f'))
+
+
+
+    gd    = np.ones(len(srcs),dtype='bool')
+
+    #Fluxs,Corfacts = SourceFlux(bandpasses[0],bandpasses[1][2,:],mjd,srcs)
+    #TDio = Fluxs/(peaks[:,2]*Corfacts)/1000.
+    #gd    = gd & (TDio < 0.69) & (TDio > 100.52)
+    
+    #Fluxs,Corfacts = SourceFlux(bandpasses[0],bandpasses[1][5,:],mjd,srcs)
+    #TDio = Fluxs/(peaks[:,5]*Corfacts)/1000.
+    #gd    = gd & (TDio > 100)
+
+    
+    crabs = np.where((srcs[gd] == 'CRAB'))[0]
+
+    print peaks.shape
+    for ch in channels:
+        Fluxs,Corfacts = SourceFlux(bandpasses[0],bandpasses[1][ch,:],mjd,srcs)
+        TDio = Fluxs/(peaks[:,ch]*Corfacts)/1000.
+
+        gd = (np.abs(TDio-np.median(TDio)) < 0.15) 
+        pfit = np.poly1d(np.polyfit(np.arange(len(TDio[gd])), TDio[gd],1))
+        gd = np.where(gd)[0]
+        
+
+        print np.median(TDio[gd]),np.std(TDio[gd])/np.median(TDio[gd]) * 100.
+
+        #print np.append(crab[:,0],casa[:,0])[(TDio[gd] > 1.31)]
+        
+        pyplot.plot(mjd[gd],TDio[gd],'o')
+        pyplot.plot(mjd[gd[crabs]],TDio[gd[crabs]],'o')
+
+        #pyplot.plot(para[gd],TDio[gd],'o')
+
+
+        pyplot.show()
+    print stop
+    
+    #for ch in channels:
+    #    print chan[ch], '&', '%2.3f' % np.median(dios[:,ch]), '\\\\'

Scripts/SF_Fit.py
@@ -21,16 +21,20 @@ import scipy.interpolate as ip
 import scipy.optimize as op
 import scipy.fftpack as sfft
 
+import healpy as hp
+
 if __name__ == "__main__":
 
     #Define data files (Change these to whatever you file structure is):
     datadir = '../DownSampData/data/'
     mfidir  = '/nas/scratch/sharper/QUIJOTE/MFI-Pipeline/MFI_INFO/'
+    foreground_mask_dir = '/nas/scratch/sharper/QUIJOTE/Pipeline/MAPS/' #Foreground mask map directory
 
     dioModel = np.loadtxt('/nas/scratch/sharper/QUIJOTE/MFI-Pipeline/MFI_INFO/DiodePolModel-Epoch2.dat')
 
     #Read in list of files (Just needs to be part of name e.g. CASS would read in all file containing substring CASS)
-    files = np.loadtxt('FileList.txt',dtype='string',ndmin=1)
+    #files = np.loadtxt('FileList.txt',dtype='string',ndmin=1)
+    files = np.loadtxt('CASSFILES',dtype='string',ndmin=1)
 
     #Read in T-Point parameters:
     tpoints = np.loadtxt(mfidir+'focal_plane.ini')
@@ -38,7 +42,31 @@ if __name__ == "__main__":
     jd0 = 56244.
 
     chanPairs = np.array([[0,6],[1,7],[2,4],[3,5]])
-    beams     = np.array([0.88,0.66,0.88,0.66])
+    beams     = [{'X':[0.89/2.355,False],'Y':[0.99*0.89/2.355,False]},
+                 {'X':[0.65/2.355,False],'Y':[0.87*0.65/2.355,False]},
+                 {'X':[0.85/2.355,False],'Y':[0.92*0.85/2.355,False]},
+                 {'X':[0.64/2.355,False],'Y':[0.99*0.64/2.355,False]}]
+
+    #Noise limits:
+    noiseLims = np.array([0.0764244215806,
+                          0.0514327622598,
+                          0.0889688890257,
+                          0.054032925059,
+                          0.00664899914188,
+                          0.014294220537,
+                          0.00656608744065,
+                          0.0119769612796,
+                          0.0599282051666,
+                          0.124438793376,
+                          0.0575965942848,
+                          0.0426464567593])*2.
+
+    
+
+    #Foreground mask:
+    foremap = hp.read_map(foreground_mask_dir+'maskmap_N512.fits')
+    foremap = (foremap == 0)
+
     
     Params = np.zeros(12)
     rFacts = np.zeros(12)
@@ -129,9 +157,9 @@ if __name__ == "__main__":
                                                                               'Pb':tpoints[(chPair[0]+horn*8)/8,9]})
 
                 #Convert RA/DEC to telescope frame:
-                sra,sdec = SourceFitting.ModelFlux.SourceCoord('CRAB')
-                #xi = (np.mod(ra*180./np.pi+180.,360.) - (sra-180.))*np.cos(dec) #!!! Remove the 180 if not fitting for Cas A!!!
-                xi = (np.mod(ra*180./np.pi,360.) - (sra))*np.cos(dec) #!!! Remove the 180 if not fitting for Cas A!!!
+                sra,sdec = SourceFitting.ModelFlux.SourceCoord('CASA')
+                xi = (np.mod(ra*180./np.pi+180.,360.) - (sra-180.))*np.cos(dec) #!!! Remove the 180 if not fitting for Cas A!!!
+                #xi = (np.mod(ra*180./np.pi,360.) - (sra))*np.cos(dec) #!!! Remove the 180 if not fitting for Cas A!!!
 
                 yi = (dec*180./np.pi-sdec) #!!! Remove the 180 if not fitting for Cas A!!!
 
@@ -141,7 +169,9 @@ if __name__ == "__main__":
 
                 beam = beams[horn]
                 
-                isNotSource  = (x**2 + y**2 > (beam*1.25)**2)    
+                isNotSource  = (x**2 + y**2 > (beam['X'][0]*1.25*2.355)**2)
+
+                stds = np.zeros(len(scanStarts))
                 
                 for i in range(len(scanStarts)-1):
                     dat = chVoltage[scanStarts[i]:scanStarts[i+1]]*1.
@@ -149,29 +179,36 @@ if __name__ == "__main__":
                     xi = x[scanStarts[i]:scanStarts[i+1]]
                     yi = y[scanStarts[i]:scanStarts[i+1]]
 
-                    isNotSource  = (xi**2 + yi**2 > (beam*1.25)**2)
+                    isNotSource  = (xi**2 + yi**2 > (beam['X'][0]*1.25*2.355)**2)
 
                     bkgdfit = np.poly1d(np.polyfit(xi[isNotSource],dat[isNotSource],3))
                 
                     chVoltage[scanStarts[i]:scanStarts[i+1]] -=bkgdfit(xi)
 
-                P = SourceFitting.lm_SourceFit.FitSource(chVoltage,x,y,beam=beam/2.355,err=chErr)
+                    stds[i] = np.std(dat[isNotSource])
 
-                if not P == None:
-                    Params[ch+(horn-1)*4] = P['amp']
-                    rFacts[ch+(horn-1)*4] = r
-                    print 'P AMP',P['amp']
+                #print 'NOISE:',np.sqrt(np.median(stds**2))
+                #pyplot.plot(stds,'o')
+                #pyplot.show()
+
+                if (np.sqrt(np.median(stds**2)) < noiseLims[ch+(horn-1)*4]):
+                    P = SourceFitting.lm_SourceFit.FitSource(chVoltage,x,y,beam=beam,err=chErr)
+
+                    if (P != None):
+                        Params[ch+(horn-1)*4] = P['amp']
+                        rFacts[ch+(horn-1)*4] = r                             
+                        print 'P AMP',P['amp']
                 else:
                     Params[ch+(horn-1)*4] = 0.
                     rFacts[ch+(horn-1)*4] = 0.
 
-        TextFiles.AppendFile('TauA',[f,
+        TextFiles.AppendFile('CasA',[f,
                                          Params[0],Params[1],Params[2],Params[3],
                                          Params[4],Params[5],Params[6],Params[7],
                                          Params[8],Params[9],Params[10],Params[11],
                                          rFacts[0],rFacts[1],rFacts[2],rFacts[3],
                                          rFacts[4],rFacts[5],rFacts[6],rFacts[7],
                                          rFacts[8],rFacts[9],rFacts[10],rFacts[11],
-                                         meanEl,meanJd])
+                                         meanEl,meanJd,np.mean(p)])
         print '--'
 

SourceFitting/lm_SourceFit.py
@@ -35,7 +35,7 @@ def residual(P,x,y,data,e):
     
     return (d - data)/e
 
-def FitSource(data,xi,yi,beam=0.92/2.355,rvals=None,filename='Out.dat',err=None):
+def FitSource(data,xi,yi,beam={'X':[0.92/2.355,True],'Y':[0.92/2.355,True]},rvals=None,filename='Out.dat',err=None):
     '''
     Return array of parameters defining a source
 
@@ -67,10 +67,10 @@ def FitSource(data,xi,yi,beam=0.92/2.355,rvals=None,filename='Out.dat',err=None)
 
     #Define some default values for rvals:
     if type(rvals) != type(dict()):
-        rvals = {'r0':beam*3.5,
-                 'r1':beam*1.175,
-                 'r2':beam*3.,
-                 'r3':beam*6.}
+        rvals = {'r0':beam['X'][0]*3.5,
+                 'r1':beam['X'][0]*1.175,
+                 'r2':beam['X'][0]*3.,
+                 'r3':beam['X'][0]*6.}
 
 
     #Define limits:
@@ -85,8 +85,8 @@ def FitSource(data,xi,yi,beam=0.92/2.355,rvals=None,filename='Out.dat',err=None)
         params = Parameters()
         params.add('bkgd',np.median(data)  )
         params.add('amp' ,np.max(data-np.median(data))  ,min=0)
-        params.add('wx'  ,beam,min=beam*0.95,max=beam*1.05,vary=True)
-        params.add('wy'  ,beam,min=beam*0.95,max=beam*1.05,vary=True)
+        params.add('wx'  ,beam['X'][0],min=beam['X'][0]*0.95,max=beam['X'][0]*1.05,vary=beam['X'][1])
+        params.add('wy'  ,beam['Y'][0],min=beam['Y'][0]*0.95,max=beam['Y'][0]*1.05,vary=beam['Y'][1])
         params.add('cx'  ,0.,vary=False)
         params.add('cy'  ,0.,vary=False)
 